c7c56ceaca125d52667ab79e6b3e1768bf2ada6a
bet.collell <bet.collelli01@estudiant.upf.edu>
2024-10-20 22:48:14 +0200

    Primer intent

--- src/antiprime.py
+++ src/antiprime.py
@@ -1,9 +1,27 @@
 ## ADD WHATEVER ARGUMENTS ARE NECESSARY TO THE MAIN FUNCTION
 ## IN THE SAME ORDER AS THE ARGUMENTS ARE TAKEN FROM THE
 ## COMMAND LINE SPECIFIED BELOW
-def main() :
+def main(x) :
 	## YOU CODE SHOULD START HERE AST THE SAME
 	## IDENTATION AS THIS COMMENT
+	r = int("0")
+	i = int("1")
+	while i <= x:
+		if x % i == 0:
+			r = r + int("1")
+		i = i + int("1")
+
+	l = x - int("1")
+	s = 0
+
+	while l >= int("1") and s < r:
+		a = int("1")
+		s = 0
+		while a <= l:
+			if l % a == 0:
+				s = s + int("1")
+			a = a + int ("1")
+		l = l - int("1")
 
 
 	## THE LAST LINES OF YOUR CODE SHOULD EITHER
@@ -11,7 +29,11 @@ def main() :
 	## REPLACE THE FOLLOWING LINE BY WHATEVER LINES
 	## OF CODE ALLOW THIS FUNCTION TO RETURN THE VALUE
 	## "anti-prime" or "not anti-prime"
-	return("anti-prime")
+	if s < r:
+		res = ("anti-prime")
+	else:
+		res = ("not anti-prime")
+	return(res)
 
 ## DO NOT REMOVE THIS LINE BELOW
 if __name__ == "__main__" :
@@ -21,4 +43,6 @@ if __name__ == "__main__" :
 	## $ python antiprime.py 6
 	## WHERE THE FIRST ARGUMENT IS A POSITIVE INTEGER NUMBER FOR WHICH
 	## YOU WANT TO FIGURE OUT WHETHER IS ANTI-PRIME OR NOT
-	print(main())
+	import sys
+	x = int (sys.argv [1])
+	print(main(x))
